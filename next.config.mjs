e24735c63cd124eeaf398bd44274719f02e5b2f9
jun <cmoonjun11@gmail.com>
2024-06-12 22:35:57 +0900

    MOD : next.config.mjs 설정 수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  compiler: {
+    styledComponents: true,
+  },
+  generateBuildId: () => nextBuildId({ dir: __dirname, describe: true }),
+  typescript: {
+    ignoreBuildErrors: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERRORS,
+  },
+};
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import * as React from "react";
-import styles from "./page.module.css";
 import { Container } from "@mui/material";
 import Main from "./main/page";
 
